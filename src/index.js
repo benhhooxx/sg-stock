5737b716bc166529fd965461d86fcd526b2ff8b1
Ben <ben.p.ho@accenture.com>
2022-02-15 21:19:03 +0800

    edit webpack config for build

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from "react-router-dom";
-import App from './App';
-import './css/styles.scss';
+require("file-loader?name=[name].[ext]!./index.html");
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter as Router } from "react-router-dom";
+import App from "./App";
+import "./css/styles.scss";
 
 ReactDOM.render(
   <React.StrictMode>
--- webpack.config.js
+++ webpack.config.js
@@ -1,77 +1,82 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const isProduction = process.env.NODE_ENV === 'production';
 
-const config = {
+module.exports = {
   entry: path.join(__dirname, "src", "index.js"),
-  output: { 
-    path: path.join(__dirname, "dist"), 
-    filename: "bundle.js",
-    clean: true
+  output: {
+    path: path.join(__dirname, "/build"),
+    filename: "index.bundle.js",
   },
-  mode: process.env.NODE_ENV || "development",
-  resolve: { modules: [path.resolve(__dirname, "src"), "node_modules"] },
-  devServer: { 
+  devServer: {
+    port: 3010,
     static: path.resolve(__dirname, "dist"),
     open: true,
-    historyApiFallback: true // solve react-router urls don't work when refreshing the page
+    historyApiFallback: true, // solve react-router urls don't work when refreshing the page
   },
+  // mode: process.env.NODE_ENV || "development",
+  // resolve: { modules: [path.resolve(__dirname, "src"), "node_modules"] },
   module: {
     rules: [
-        { 
-          test: /\.(js|jsx)$/, 
-          exclude: /node_modules/, 
-          use: [{
-            loader: 'babel-loader',
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
             options: {
-                cacheDirectory: true,
-                plugins: ['@babel/plugin-transform-runtime']
-            }
-          }]
-        },
-        {
-          test: /\.s[ac]ss$/i,
-          use: [
-            // Creates `style` nodes from JS strings
-            "style-loader", 
-            // Translates CSS into CommonJs
-            {
-              loader: "css-loader", options: { importLoaders: 1 }
+              cacheDirectory: true,
+              plugins: ["@babel/plugin-transform-runtime"],
             },
-            // Compiles Sass to CSS
-            {
-              loader: 'sass-loader',
-              options: {
-                webpackImporter: false,
-                sassOptions: {
-                  includePaths: ['node_modules'],
-                },
-                // Prefer `dart-sass`
-                implementation: require("sass"),
+          },
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJs
+          {
+            loader: "css-loader",
+            options: { importLoaders: 1 },
+          },
+          // Compiles Sass to CSS
+          {
+            loader: "sass-loader",
+            options: {
+              webpackImporter: false,
+              sassOptions: {
+                includePaths: ["node_modules"],
               },
+              // Prefer `dart-sass`
+              implementation: require("sass"),
             },
-            "postcss-loader"
-          ],
-        },
-        { 
-          test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
-          use: ["file-loader"] 
-        },
+          },
+          "postcss-loader",
+        ],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: ["file-loader"],
+      },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "SG Stock Webapp",
-      template: path.join(__dirname, "dist", "index.html"),
-    }),
-  ],
+  // plugins: [
+  //   new HtmlWebpackPlugin({
+  //     title: "SG Stock Webapp",
+  //     template: path.join(__dirname, "dist", "index.html"),
+  //   }),
+  // ],
+  // plugins: [new MiniCssExtractPlugin()],
 };
 
-module.exports = () => {
-    if (isProduction) {
-      config.mode = 'production';
-    } else {
-      config.mode = 'development';
-    }
-    return config;
-};
+// module.exports = () => {
+//     if (isProduction) {
+//       config.mode = 'production';
+//     } else {
+//       config.mode = 'development';
+//     }
+//     return config;
+// };

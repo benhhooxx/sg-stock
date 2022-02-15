ffeb5d329d8b85ae9e42070e95755a92cae6c6b4
Ben <benhhooxx@gmail.com>
2022-02-15 22:25:49 +0800

    fix sidebar

--- src/partials/Sidebar.js
+++ src/partials/Sidebar.js
@@ -48,15 +48,21 @@ function Sidebar({
   return (
     <div>
       {/* Sidebar backdrop (mobile only) */}
-      <div className={`fixed inset-0 bg-gray-900 bg-opacity-30 z-40 lg:hidden lg:z-auto transition-opacity duration-200 ${sidebarOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`} aria-hidden="true"></div>
+      <div
+        className={`fixed inset-0 bg-gray-900 bg-opacity-30 z-40 lg:hidden lg:z-auto transition-opacity duration-200 ${
+          sidebarOpen ? "opacity-100" : "opacity-0 pointer-events-none"
+        }`}
+        aria-hidden="true"
+      ></div>
 
       {/* Sidebar */}
       <div
         id="sidebar"
         ref={sidebar}
-        className={`flex flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 transform h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-20 lg:sidebar-expanded:!w-64 2xl:!w-64 flex-shrink-0 bg-gray-800 p-4 transition-all duration-200 ease-in-out ${sidebarOpen ? 'translate-x-0' : '-translate-x-64'}`}
+        className={`flex flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 transform h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-20 lg:sidebar-expanded:!w-64 2xl:!w-64 flex-shrink-0 bg-gray-800 p-4 transition-all duration-200 ease-in-out ${
+          sidebarOpen ? "translate-x-0" : "-translate-x-64"
+        }`}
       >
-
         {/* Sidebar header */}
         <div className="flex justify-between mb-10 pr-3 sm:px-2">
           {/* Close button */}
@@ -68,28 +74,54 @@ function Sidebar({
             aria-expanded={sidebarOpen}
           >
             <span className="sr-only">Close sidebar</span>
-            <svg className="w-6 h-6 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <svg
+              className="w-6 h-6 fill-current"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
               <path d="M10.7 18.7l1.4-1.4L7.8 13H20v-2H7.8l4.3-4.3-1.4-1.4L4 12z" />
             </svg>
           </button>
           {/* Logo */}
-          <NavLink exact="true" to="/" className="block">
+          <NavLink exact="true" to="/" className="inline-flex items-center">
             <svg width="32" height="32" viewBox="0 0 32 32">
               <defs>
-                <linearGradient x1="28.538%" y1="20.229%" x2="100%" y2="108.156%" id="logo-a">
+                <linearGradient
+                  x1="28.538%"
+                  y1="20.229%"
+                  x2="100%"
+                  y2="108.156%"
+                  id="logo-a"
+                >
                   <stop stopColor="#A5B4FC" stopOpacity="0" offset="0%" />
                   <stop stopColor="#A5B4FC" offset="100%" />
                 </linearGradient>
-                <linearGradient x1="88.638%" y1="29.267%" x2="22.42%" y2="100%" id="logo-b">
+                <linearGradient
+                  x1="88.638%"
+                  y1="29.267%"
+                  x2="22.42%"
+                  y2="100%"
+                  id="logo-b"
+                >
                   <stop stopColor="#38BDF8" stopOpacity="0" offset="0%" />
                   <stop stopColor="#38BDF8" offset="100%" />
                 </linearGradient>
               </defs>
               <rect fill="#6366F1" width="32" height="32" rx="16" />
-              <path d="M18.277.16C26.035 1.267 32 7.938 32 16c0 8.837-7.163 16-16 16a15.937 15.937 0 01-10.426-3.863L18.277.161z" fill="#4F46E5" />
-              <path d="M7.404 2.503l18.339 26.19A15.93 15.93 0 0116 32C7.163 32 0 24.837 0 16 0 10.327 2.952 5.344 7.404 2.503z" fill="url(#logo-a)" />
-              <path d="M2.223 24.14L29.777 7.86A15.926 15.926 0 0132 16c0 8.837-7.163 16-16 16-5.864 0-10.991-3.154-13.777-7.86z" fill="url(#logo-b)" />
+              <path
+                d="M18.277.16C26.035 1.267 32 7.938 32 16c0 8.837-7.163 16-16 16a15.937 15.937 0 01-10.426-3.863L18.277.161z"
+                fill="#4F46E5"
+              />
+              <path
+                d="M7.404 2.503l18.339 26.19A15.93 15.93 0 0116 32C7.163 32 0 24.837 0 16 0 10.327 2.952 5.344 7.404 2.503z"
+                fill="url(#logo-a)"
+              />
+              <path
+                d="M2.223 24.14L29.777 7.86A15.926 15.926 0 0132 16c0 8.837-7.163 16-16 16-5.864 0-10.991-3.154-13.777-7.86z"
+                fill="url(#logo-b)"
+              />
             </svg>
+            <h1 className="text-white ml-2">SG Stock Webapp</h1>
           </NavLink>
         </div>
 
@@ -98,33 +130,95 @@ function Sidebar({
           {/* Pages group */}
           <div>
             <h3 className="text-xs uppercase text-gray-500 font-semibold pl-3">
-              <span className="hidden lg:block lg:sidebar-expanded:hidden 2xl:hidden text-center w-6" aria-hidden="true">•••</span>
-              <span className="lg:hidden lg:sidebar-expanded:block 2xl:block">Pages</span>
+              <span
+                className="hidden lg:block lg:sidebar-expanded:hidden 2xl:hidden text-center w-6"
+                aria-hidden="true"
+              >
+                •••
+              </span>
+              <span className="lg:hidden lg:sidebar-expanded:block 2xl:block">
+                Pages
+              </span>
             </h3>
             <ul className="mt-3">
               {/* Dashboard */}
-              <li className={`px-3 py-2 rounded-sm mb-0.5 last:mb-0 ${pathname === '/' && 'bg-gray-900'}`}>
-                <NavLink exact="true" to="/" className={`block text-gray-200 hover:text-white truncate transition duration-150 ${pathname === '/' && 'hover:text-gray-200'}`}>
+              <li
+                className={`px-3 py-2 rounded-sm mb-0.5 last:mb-0 ${
+                  pathname === "/" && "bg-gray-900"
+                }`}
+              >
+                <NavLink
+                  exact="true"
+                  to="/"
+                  className={`block text-gray-200 hover:text-white truncate transition duration-150 ${
+                    pathname === "/" && "hover:text-gray-200"
+                  }`}
+                >
                   <div className="flex items-center">
                     <svg className="flex-shrink-0 h-6 w-6" viewBox="0 0 24 24">
-                      <path className={`fill-current text-gray-400 ${pathname === '/' && '!text-indigo-500'}`} d="M12 0C5.383 0 0 5.383 0 12s5.383 12 12 12 12-5.383 12-12S18.617 0 12 0z" />
-                      <path className={`fill-current text-gray-600 ${pathname === '/' && 'text-indigo-600'}`} d="M12 3c-4.963 0-9 4.037-9 9s4.037 9 9 9 9-4.037 9-9-4.037-9-9-9z" />
-                      <path className={`fill-current text-gray-400 ${pathname === '/' && 'text-indigo-200'}`} d="M12 15c-1.654 0-3-1.346-3-3 0-.462.113-.894.3-1.285L6 6l4.714 3.301A2.973 2.973 0 0112 9c1.654 0 3 1.346 3 3s-1.346 3-3 3z" />
+                      <path
+                        className={`fill-current text-gray-400 ${
+                          pathname === "/" && "!text-indigo-500"
+                        }`}
+                        d="M12 0C5.383 0 0 5.383 0 12s5.383 12 12 12 12-5.383 12-12S18.617 0 12 0z"
+                      />
+                      <path
+                        className={`fill-current text-gray-600 ${
+                          pathname === "/" && "text-indigo-600"
+                        }`}
+                        d="M12 3c-4.963 0-9 4.037-9 9s4.037 9 9 9 9-4.037 9-9-4.037-9-9-9z"
+                      />
+                      <path
+                        className={`fill-current text-gray-400 ${
+                          pathname === "/" && "text-indigo-200"
+                        }`}
+                        d="M12 15c-1.654 0-3-1.346-3-3 0-.462.113-.894.3-1.285L6 6l4.714 3.301A2.973 2.973 0 0112 9c1.654 0 3 1.346 3 3s-1.346 3-3 3z"
+                      />
                     </svg>
-                    <span className="text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Stock List</span>
+                    <span className="text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                      Stock List
+                    </span>
                   </div>
                 </NavLink>
               </li>
               {/* Analytics */}
-              <li className={`px-3 py-2 rounded-sm mb-0.5 last:mb-0 ${pathname.includes('orders-basket') && 'bg-gray-900'}`}>
-                <NavLink exact="true" to="/orders-basket" className={`block text-gray-200 hover:text-white truncate transition duration-150 && 'hover:text-gray-200'}`}>
+              <li
+                className={`px-3 py-2 rounded-sm mb-0.5 last:mb-0 ${
+                  pathname.includes("orders-basket") && "bg-gray-900"
+                }`}
+              >
+                <NavLink
+                  exact="true"
+                  to="/orders-basket"
+                  className={`block text-gray-200 hover:text-white truncate transition duration-150 && 'hover:text-gray-200'}`}
+                >
                   <div className="flex items-center">
                     <svg className="flex-shrink-0 h-6 w-6" viewBox="0 0 24 24">
-                      <path className={`fill-current text-gray-400 ${pathname.includes('orders-basket') && 'text-indigo-300'}`} d="M13 15l11-7L11.504.136a1 1 0 00-1.019.007L0 7l13 8z" />
-                      <path className={`fill-current text-gray-700 ${pathname.includes('orders-basket') && '!text-indigo-600'}`} d="M13 15L0 7v9c0 .355.189.685.496.864L13 24v-9z" />
-                      <path className={`fill-current text-gray-600 ${pathname.includes('orders-basket') && 'text-indigo-500'}`} d="M13 15.047V24l10.573-7.181A.999.999 0 0024 16V8l-11 7.047z" />
+                      <path
+                        className={`fill-current text-gray-400 ${
+                          pathname.includes("orders-basket") &&
+                          "text-indigo-300"
+                        }`}
+                        d="M13 15l11-7L11.504.136a1 1 0 00-1.019.007L0 7l13 8z"
+                      />
+                      <path
+                        className={`fill-current text-gray-700 ${
+                          pathname.includes("orders-basket") &&
+                          "!text-indigo-600"
+                        }`}
+                        d="M13 15L0 7v9c0 .355.189.685.496.864L13 24v-9z"
+                      />
+                      <path
+                        className={`fill-current text-gray-600 ${
+                          pathname.includes("orders-basket") &&
+                          "text-indigo-500"
+                        }`}
+                        d="M13 15.047V24l10.573-7.181A.999.999 0 0024 16V8l-11 7.047z"
+                      />
                     </svg>
-                    <span className="text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Orders Basket</span>
+                    <span className="text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                      Orders Basket
+                    </span>
                   </div>
                 </NavLink>
               </li>
@@ -137,14 +231,19 @@ function Sidebar({
           <div className="px-3 py-2">
             <button onClick={() => setSidebarExpanded(!sidebarExpanded)}>
               <span className="sr-only">Expand / collapse sidebar</span>
-              <svg className="w-6 h-6 fill-current sidebar-expanded:rotate-180" viewBox="0 0 24 24">
-                <path className="text-gray-400" d="M19.586 11l-5-5L16 4.586 23.414 12 16 19.414 14.586 18l5-5H7v-2z" />
+              <svg
+                className="w-6 h-6 fill-current sidebar-expanded:rotate-180"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  className="text-gray-400"
+                  d="M19.586 11l-5-5L16 4.586 23.414 12 16 19.414 14.586 18l5-5H7v-2z"
+                />
                 <path className="text-gray-600" d="M3 23H1V1h2z" />
               </svg>
             </button>
           </div>
         </div>
-
       </div>
     </div>
   );
